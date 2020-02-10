f956620cdd04e1284801d01164cf0779a00fe6b6
gupiccolimartins <gpiccolimartins@gmail.com>
2020-02-10 17:17:13 -0300

    metade da aula de componentes de classe

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -1,13 +1,7 @@
-System.register(["../views/index", "../models/index", "../helpers/decorators/index"], function (exports_1, context_1) {
+System.register(["../views/index", "../models/index"], function (exports_1, context_1) {
     "use strict";
-    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-        return c > 3 && r && Object.defineProperty(target, key, r), r;
-    };
     var __moduleName = context_1 && context_1.id;
-    var index_1, index_2, index_3, NegociacaoController, DiaDaSemana;
+    var index_1, index_2, NegociacaoController, DiaDaSemana;
     return {
         setters: [
             function (index_1_1) {
@@ -15,9 +9,6 @@ System.register(["../views/index", "../models/index", "../helpers/decorators/ind
             },
             function (index_2_1) {
                 index_2 = index_2_1;
-            },
-            function (index_3_1) {
-                index_3 = index_3_1;
             }
         ],
         execute: function () {
@@ -26,9 +17,6 @@ System.register(["../views/index", "../models/index", "../helpers/decorators/ind
                     this._negociacoes = new index_2.Negociacoes;
                     this._negociacoesView = new index_1.NegociacoesView('#negociacoesView');
                     this._mensagemView = new index_1.MensagemView('#mensagemView');
-                    this._inputData = $('#data');
-                    this._inputQuantidade = $('#quantidade');
-                    this._inputValor = $('#valor');
                     this._negociacoesView.update(this._negociacoes);
                 }
                 adiciona(event) {
@@ -47,9 +35,6 @@ System.register(["../views/index", "../models/index", "../helpers/decorators/ind
                     return data.getDay() != DiaDaSemana.Domingo && data.getDay() != DiaDaSemana.Sabado;
                 }
             };
-            __decorate([
-                index_3.logarTempoDeExecucao()
-            ], NegociacaoController.prototype, "adiciona", null);
             exports_1("NegociacaoController", NegociacaoController);
             (function (DiaDaSemana) {
                 DiaDaSemana[DiaDaSemana["Domingo"] = 0] = "Domingo";

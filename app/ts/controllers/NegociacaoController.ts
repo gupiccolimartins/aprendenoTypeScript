d51505fb5baaa5e5b91b5ae2d9690683451347f7
gupiccolimartins <gpiccolimartins@gmail.com>
2020-02-10 09:36:10 -0300

    Apresentando o modelo para o usuário

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -4,12 +4,14 @@ class NegociacaoController {
     private _inputQuantidade :HTMLInputElement;
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes;
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
 
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._negociacoesView.update();
 
     }
 
@@ -26,5 +28,10 @@ class NegociacaoController {
         );
 
         this._negociacoes.adiciona(negociacao);
+
+        this._negociacoes.paraArray().forEach(negociacao => {
+
+            console.log(negociacao);
+        });
     }
 }
